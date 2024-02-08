31fa7883148e3ad0a54dc140904b5d0e160c15f9
paklindsey <paklindsey@gmail.com>
2024-02-08 14:17:52 -0500

    recipe page cards

--- src/components/recipes/Main.js
+++ src/components/recipes/Main.js
@@ -1,31 +1,30 @@
-import { useEffect, useContext } from "react";
-import Context from "../../context/Context";
+import { useEffect, useState } from "react";
 import { onSnapshot, collection } from "firebase/firestore";
 import "../../styles/recipes.scss";
-import RecipeCard from "./RecipeCard";
 import db from "../../config/firebase";
 
 const RecipesMain = () => {
-  const { data, setData } = useContext(Context);
-  // const [recipes, setRecipes] = useState([
-  //   { name: "Loading...", id: "initial" },
-  // ]);
+  const [popoutOpen, setPopoutOpen] = useState(false);
+  const [recipes, setRecipes] = useState([
+    { name: "Loading...", id: "initial" },
+  ]);
   useEffect(
     () =>
       onSnapshot(collection(db, "recipes"), (snapshot) =>
-        setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        setRecipes(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
       ),
     []
   );
-  console.log("data", data);
+
   return (
-    <section className="recipesMain">
-      <RecipeCard index={0} />
-      <RecipeCard index={1} />
-      <RecipeCard index={2} />
-      <RecipeCard index={3} />
-      <RecipeCard index={4} />
-      <RecipeCard index={5} />
+    <section className="recipes">
+      {recipes &&
+        recipes.map((card) => (
+          <div className="recipes__card" key={card.id}>
+            <h6>{card.name}</h6>
+            <img src={card.image} alt="food" />
+          </div>
+        ))}
     </section>
   );
 };
